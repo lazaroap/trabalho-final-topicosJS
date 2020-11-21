7f2896ab0ffb826b4c2c5520385f8fdc7fd141a7
thais <thaisknuth@gmail.com>
2020-11-21 16:51:51 -0300

    :wrench: Atualiza configs para migraçoes e cache

--- ormconfig.js
+++ ormconfig.js
@@ -1,18 +1,18 @@
-//ormconfig.js:
 module.exports = {
-  type: 'postgres',
-  host: process.env.DB_HOST,
-  port: +process.env.DB_PORT || 5432,
-  username: process.env.DB_USERNAME,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  entities:['dist/**/*.entity{.ts,.js}'],
-  synchronize: process.env.DB_SYNC == 'true',
-  migrations: ['./dist/database/migrations/**/*.ts'],
-  cli: {
-    entitiesDir: ['./dist/**/*.entity'],
-    migrationsDir: ['./dist/database/migrations/'],
+  "type": 'postgres',
+  "host": process.env.DB_HOST,
+  "port": +process.env.DB_PORT || 5432,
+  "username": process.env.DB_USERNAME,
+  "password": process.env.DB_PASSWORD,
+  "database": process.env.DB_NAME,
+  "cache":{duration:20000}, //tempo de vida do cache
+  "entities": ['dist/**/*.entity{.ts,.js}'],
+  "migrations": ['dist/database/migrations/**/*.js'],
+  "synchronized":false,
+  "cli": {
+    "entitiesDir": ['src/**/*.entity'],
+    "migrationsDir": ['src/database/migrations/'],
   },
-  migrationsTableName: "migrations_typeorm",
-  migrationsRun: true,
+  "migrationsTableName": "migrations_typeorm",
+  "migrationsRun": true,
 }; 
